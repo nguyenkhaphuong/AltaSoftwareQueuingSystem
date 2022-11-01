d3434577e26536a314d8285e4e1aef4166c2ed06
Nguyễn Khả Phương <phuongkha1506@gmail.com>
2022-11-01 15:23:36 +0700

    Update Nov 1st, 2022

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Login from "./pages/Login/Login";
+import Profile from "./pages/Profile/Profile";
 
 function App() {
   return (
     <div>
-      <Login />
+      {/* <Login /> */}
+      <Profile />
     </div>
   );
 }
